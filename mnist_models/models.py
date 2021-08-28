186c29ea3c431f63a52ded1e7b5f061c0b3f65a7
Kevin Nowland <kevin.nowland@gmail.com>
2021-08-28 17:13:53 -0400

    add model code

--- mnist_models/models.py
+++ mnist_models/models.py
@@ -1,16 +1,63 @@
 """ module of classes holding modles, pretrained or otherwise """
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
 
 class MnistModel:
     """ class for models """
-    pass
+
+    def __init__(self, model):
+        self.__model = model
+        self.__accuracy = None
+        self.__is_trained = False
+
+    @property
+    def model(self):
+        """ the model itself """
+        return self.__model
+
+    @property
+    def accuracy(self):
+        """ the accuracy, once fit """
+        return self.__accuracy
+
+    @property
+    def is_trained(self):
+        return self.__is_trained
+
+    def fit(self, X_train, y_train, X_test, y_test):
+        """ fit the model and record accuracy from test set"""
+
+        if self.model is not None:
+            self.model.fit(X_train, y_train)
+            y_pred = self.model.predict(X_test)
+            self.__accuracy = accuracy_score(y_test, y_pred)
+            self.__is_trained = True
+
+    def predict(self, X):
+        """ predict with the model """
+
+        if self.model is not None:
+            return self.model.predict(X)
 
 
 class SVMModel(MnistModel):
     """ class for the SVM model """
-    pass
+
+    def __init__(self):
+        super().__init__(SVC())
+
+    def __str__(self):
+        return "SVMModel(is_trained={})".format(self.is_trained)
 
 
 class LogisticModel(MnistModel):
     """ class for logistic regression model  """
-    pass
+
+    def __init__(self):
+        super().__init__(LogisticRegression())
+
+    def __str__(self):
+        return "LogisticModel(is_trained={})".format(self.is_trained)
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,9 +3,53 @@
 import context
 context.init()
 
-import mnist_models.models
+import mnist_models.models as mo
+import mnist_models.data as da
+import numpy as np
+from sklearn.dummy import DummyClassifier
 
 
-def test_hello_World():
+X_train_, y_train_ = da.train_data([0, 1])
+X_test_, y_test_ = da.test_data([0, 1])
 
-    print("Hello, World!")
+X_train = X_train_[:40]
+y_train = y_train_[:40]
+X_test = X_test_[:10]
+y_test = y_test_[:10]
+
+
+def test_base_class():
+    """ test the base class works """
+
+    model = mo.MnistModel(DummyClassifier())
+
+    assert model.accuracy is None, "accuracy not None"
+    assert not model.is_trained, "model claims it is trained before training"
+
+    model.fit(X_train, y_train, X_test, y_test)
+
+    assert model.accuracy is not None, "accuracy none after fitting"
+    assert model.is_trained, "model claims it is not trained after training"
+
+    X_test_ = X_test[:2]
+    y_pred = model.predict(X_test_)
+
+    assert y_pred.shape[0] == X_test_.shape[0], "prediction shape not matching"
+
+
+def test_svm():
+
+    model = mo.SVMModel()
+    model.fit(X_train, y_train, X_test, y_test)
+    y_pred = model.predict(X_test)
+
+    assert y_pred.shape[0] == X_test.shape[0], "prediction shape not matching"
+
+
+def test_logistic():
+
+    model = mo.LogisticModel()
+    model.fit(X_train, y_train, X_test, y_test)
+    y_pred = model.predict(X_test)
+
+    assert y_pred.shape[0] == X_test.shape[0], "prediction shape not matching"
