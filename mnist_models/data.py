8505f11675cdb737fb11994a058a4bb2bc9d3f47
Kevin Nowland <kevin.nowland@gmail.com>
2021-08-28 17:25:10 -0400

    normalize data

--- mnist_models/data.py
+++ mnist_models/data.py
@@ -22,7 +22,7 @@ def _get_data(train_or_test, digits):
     images_reshaped = images.reshape(images.shape[0],
                                      images.shape[1] * images.shape[2])
 
-    return images_reshaped, labels
+    return images_reshaped / 255.0, labels
 
 
 def train_data(digits=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]):
