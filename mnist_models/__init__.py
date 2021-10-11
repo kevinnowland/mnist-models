f3f9291eef393c283c87c82a4fb82b1fb459a775
Kevin Nowland <kevin.nowland@gmail.com>
2021-10-11 19:53:21 -0400

    add imports to __init__.py

---
+++ mnist_models/__init__.py
@@ -0,0 +1,6 @@
+from .data import train_data, test_data
+from .models import MnistModel, SVMModel, LogisticModel
+from .load_models import (load_full_logistic,
+                          load_zero_one_logistic,
+                          load_full_svm,
+                          load_zero_one_svm)
